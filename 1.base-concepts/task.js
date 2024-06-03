348f5e587d4f9ec63606818ebb201b814dd96b16
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-03 14:25:03 +0300

    cheange task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let discriminant = b ** 2 - 4 * a * c;
+  let x1, x2;
+
+  if (discriminant < 0) {
+    return []; // корней нет
+  } else if (discriminant === 0) {
+    x1 = -b / (2 * a);
+    return [x1]; // один корень
+  } else {
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [x1, x2]; // два корня
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
