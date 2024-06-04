95b56afb79840fd752c143d8875a39806b768490
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-04 17:16:03 +0300

    cheange task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,31 @@
 function getArrayParams(...arr) {
-  const min = Math.min(...arr); 
-  const max = Math.max(...arr); 
-  const sum = arr.reduce((acc, curr) => acc + curr, 0); 
-  const avg = (sum / arr.length).toFixed(2); 
-  return { min, max, avg: Number(avg) }; 
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const sum = arr.reduce((acc, curr) => acc + curr, 0);
+  const avg = (sum / arr.length).toFixed(2);
+  return {
+    min,
+    max,
+    avg: Number(avg)
+  };
 }
 
 function summElementsWorker(...arr) {
- if (arr.length === 0) return 0;
- return arr.reduce((acc, curr) => acc + curr, 0);
+  if (arr.length === 0) return 0;
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
- if (arr.length === 0) return 0;
+  if (arr.length === 0) return 0;
   const max = Math.max(...arr);
   const min = Math.min(...arr);
- return max - min;
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-  if (arr.length === 0) return 0; 
-    let sumEvenElement = 0;
-    let sumOddElement = 0;
+  if (arr.length === 0) return 0;
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
   for (let num of arr) {
     if (num % 2 === 0) {
       sumEvenElement += num;
@@ -34,8 +38,8 @@ function differenceEvenOddWorker(...arr) {
 
 function averageEvenElementsWorker(...arr) {
   if (arr.length === 0) return 0;
-    let sumEvenElement = 0;
-    let countEvenElement = 0;
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
   for (let num of arr) {
     if (num % 2 === 0) {
       sumEvenElement += num;
@@ -46,7 +50,7 @@ function averageEvenElementsWorker(...arr) {
   return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
     const result = func(...arrOfArr[i]);
