1637d3329d23d966707d4f8dc4605fda74a30ff5
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-10 15:00:07 +0300

    cheange task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length && arr1.every((item, idx) => item ===arr2[idx])
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let mas =users
+      .filter((item) => item.gender === gender)
+      .map((item) => item.age);
+  let res = mas.reduce((sum, age) => sum = age, 0);
+
+  return mas.length > 0 ? res / mas.length : 0;
 }
