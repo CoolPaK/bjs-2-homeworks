f991fa898fa13d2f74d14b255103bf46eb9ec644
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-10 16:35:50 +0300

    cheange task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,24 @@
 function compareArrays(arr1, arr2) {
-  return (
-    arr1.length === arr2.length && arr1.every((item, idx) => item ===arr2[idx])
-  );
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]); // решение   
+}
+module.exports = {
+  compareArrays
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  let mas =users
-      .filter((item) => item.gender === gender)
-      .map((item) => item.age);
-  let res = mas.reduce((sum, age) => sum = age, 0);
+  const mas = users.filter(el => el.gender === gender).map(el => el.age);
 
-  return mas.length > 0 ? res / mas.length : 0;
+  const averageAge = mas.reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
+
+  return averageAge;
+}
+
+module.exports = {
+  getUsersNamesInAgeRange
 }
