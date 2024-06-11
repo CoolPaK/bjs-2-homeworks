f084c77880241c3a3623cb229afe029fef08a4bc
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-11 14:46:36 +0300

    cheange task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,39 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
-  this.marks = [];
+    this.marks = []; // Свойство marks с пустым массивом для хранения оценок
 }
 
+// Добавляем метод setSubject в прототип Student
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
-}
+    this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  if (this.marks) (
-    this.marks.push(...marks)
-  )
-}
+// Добавляем метод addMarks для добавления оценок
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd);
+    }
+};
 
+// Добавляем метод getAverage для вычисления среднего арифметического оценок
 Student.prototype.getAverage = function () {
-  if (this.marks && this.marks.length > 0) {
-    let sum = this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    return sum / this.marks.length;
-  } else {
-    return 0;
-  }
-}
+    if (this.marks && this.marks.length > 0) {
+        let sum = this.marks.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0,
+        );
+        return sum / this.marks.length;
+    } else {
+        return 0;
+    }
+};
 
+// Добавляем метод exclude для исключения студента из учебного процесса
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
-}
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
