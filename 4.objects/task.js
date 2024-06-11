2f030c23b16c72fd6e6deebcebe196eed26a68a9
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-11 14:52:10 +0300

    cheange task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,22 +3,19 @@ function Student(name, gender, age) {
     this.gender = gender;
     this.age = age;
 
-    this.marks = []; // Свойство marks с пустым массивом для хранения оценок
+    this.marks = [];
 }
 
-// Добавляем метод setSubject в прототип Student
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 };
 
-// Добавляем метод addMarks для добавления оценок
 Student.prototype.addMarks = function (...marksToAdd) {
     if (this.marks) {
         this.marks.push(...marksToAdd);
     }
 };
 
-// Добавляем метод getAverage для вычисления среднего арифметического оценок
 Student.prototype.getAverage = function () {
     if (this.marks && this.marks.length > 0) {
         let sum = this.marks.reduce(
@@ -31,7 +28,6 @@ Student.prototype.getAverage = function () {
     }
 };
 
-// Добавляем метод exclude для исключения студента из учебного процесса
 Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
