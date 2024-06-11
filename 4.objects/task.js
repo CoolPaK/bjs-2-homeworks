b28b3a8388a35a3a86e82e62bf4f9ca9fc826930
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-11 14:34:41 +0300

    cheange task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks) (
+    this.marks.push(...marksToAdd)
+  )
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks && this.marks.length > 0) {
+    let sum = this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
