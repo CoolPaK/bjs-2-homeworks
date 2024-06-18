6e6bde136d1512f9d825e4bb4c214cbb7b9b9a1c
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-18 14:50:08 +0300

    cheange task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -21,35 +21,34 @@ function validateCount(value) {
 //Задание 2.
 class Triangle {
     constructor(a, b, c) {
-        if (a + b <= c || a + c <= b || b + c <= a) {
-            throw new Error('Треугольник с такими сторонами не существует');
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
         }
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    get perimeter() {
+    getPerimeter() {
         return this.a + this.b + this.c;
     }
 
-    get area() {
-        const semiPerimeter = this.perimeter / 2;
-        const area = Math.sqrt(semiPerimeter * (semiPerimeter - this.a) * (semiPerimeter - this.b) * (semiPerimeter - this.c));
-        return area.toFixed(3);
+    getArea() {
+        let halfPerimeter = this.getPerimeter() / 2;
+        return Number(Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3));
     }
 }
 
 function getTriangle(a, b, c) {
     try {
         return new Triangle(a, b, c);
-    } catch (error) {
+    } catch (e) {
         return {
-            get area() {
-                return 'Ошибка! Треугольник не существует';
+            getArea() {
+                return "Ошибка! Треугольник не существует";
             },
-            get perimeter() {
-                return 'Ошибка! Треугольник не существует';
+            getPerimeter() {
+                return "Ошибка! Треугольник не существует";
             }
         };
     }
