c70b20e04286327ecda64641282e8d28a9f15d29
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-06-25 10:36:17 +0300

    cheange task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,40 +1,44 @@
 class AlarmClock {
     constructor() {
         this.alarmCollection = [];
+
         this.intervalId = null;
     }
 
     addClock(time, callback) {
         if (!time || !callback) {
             throw new Error("Отсутствуют обязательные аргументы");
-        } else if (this.alarmCollection.find((setup) => setup.time === time)) {
+        }
+
+        if (this.alarmCollection.some((alarm) => alarm.time === time)) {
             console.warn("Уже присутствует звонок на это же время");
         }
+
         this.alarmCollection.push({ time, callback, canCall: true });
     }
 
     removeClock(time) {
         this.alarmCollection = this.alarmCollection.filter(
-            (setup) => setup.time !== time,
+            (alarm) => alarm.time !== time,
         );
     }
 
     getCurrentFormattedTime() {
-        return new Date().toLocaleTimeString().slice(0, -3);
+        const now = new Date();
+        return `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}`;
     }
 
     start() {
         if (this.intervalId) {
             return;
         }
+
         this.intervalId = setInterval(() => {
-            this.alarmCollection.forEach((setup) => {
-                if (
-                    setup.time === this.getCurrentFormattedTime() &&
-                    setup.canCall
-                ) {
-                    setup.canCall = false;
-                    setup.callback();
+            const currentTime = this.getCurrentFormattedTime();
+            this.alarmCollection.forEach((alarm) => {
+                if (alarm.time === currentTime && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
                 }
             });
         }, 1000);
@@ -46,7 +50,9 @@ class AlarmClock {
     }
 
     resetAllCalls() {
-        this.alarmCollection.forEach((setup) => (setup.canCall = true));
+        this.alarmCollection.forEach((alarm) => {
+            alarm.canCall = true;
+        });
     }
 
     clearAlarms() {
